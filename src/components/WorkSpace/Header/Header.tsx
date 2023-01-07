09b9f2d20152b1e8018eb21bcdb5c1923e5b699d
login <egormuhaev@gmail.com>
2023-01-07 16:14:44 +0300

    HeaderByGroup

--- src/components/WorkSpace/Header/Header.tsx
+++ src/components/WorkSpace/Header/Header.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { useAppSelector } from "../../../hook/redux";
 import style from "./Header.module.scss";
+import HeaderByGroup from "./HeaderByGroup/HeaderByGroup";
 
 const Header = () => {
   const { text } = useAppSelector((state) => state.mainAppReducer.header);
   return (
     <div className={style.wrapper}>
       <p>{text}</p>
-
-      <div className={style.defaultDiv}></div>
+      <HeaderByGroup />
     </div>
   );
 };
---
+++ src/components/WorkSpace/Header/HeaderByGroup/HeaderByGroup.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import style from "./HeaderByGroup.module.scss"
+
+const HeaderByGroup = () => {
+    return <div className={style.wrapper}></div>
+}
+
+
+export default HeaderByGroup
